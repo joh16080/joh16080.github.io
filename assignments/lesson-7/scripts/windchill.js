20a488113bed404fd981bc7563dde465172131fe
Heather Johnson <joh16080@byui.edu>
2018-06-07 22:37:36 -0600

    more lesson 7 work

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -6,6 +6,11 @@ var windspeed = parseInt(document.getElementById("windspeed").innerHTML);
 
 var thetemp = hightemp + lowtemp / 2;
 
-var windchillfactor = 35.74 + (0.6215 * thetemp) - 35.75 * (Math.pow(windspeed, 0.16)) + 0.4275 * tempF * (Math.pow(windspeed, 0.16));
+var windchill = windchillfactor(thetemp, windspeed);
 
 document.getElementById("windchill").innerHTML = windchillfactor;
+
+function windchillfactor(thetemp, windspeed){
+var windchill = 35.74 + (0.6215 * thetemp) - 35.75 * (Math.pow(windspeed, 0.16)) + 0.4275 * tempF * (Math.pow(windspeed, 0.16));
+return windchillfactor;
+}

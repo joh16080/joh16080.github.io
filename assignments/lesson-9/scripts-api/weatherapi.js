e79aae2c9fee7f5f4cdb362215d864a2b57c1e9a
Heather Johnson <joh16080@byui.edu>
2018-06-21 16:58:10 -0600

    api

--- assignments/lesson-9/scripts-api/weatherapi.js
+++ assignments/lesson-9/scripts-api/weatherapi.js
@@ -1,15 +1,26 @@
 //connection to the API//
 let weatherRequest = new XMLHttpRequest();
-let apiURL = 'http://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=7ae51efa9b789abd3dfb69401f1cce74';
-weatherRequest.open(GET, apiURL, true);
+let apiURL = 'https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=7ae51efa9b789abd3dfb69401f1cce74';
+weatherRequest.open('GET', apiURL, true);
 weatherRequest.send();
 
 weatherRequest.onload = function() {
   let weatherData = JSON.parse(weatherRequest.responseText);
-  console.log(weatherData);
+  //console.log(weatherData);
+ 
+  document.getElementById('current-temp').innerHTML =  weatherData.main.temp;
+  
+  let imagesrc = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + 'png';
+  
+  document.getElementById('weatherimage').src = imagesrc;
 }
 
-let weatherSection = document.querySelector('');
 
 
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={7ae51efa9b789abd3dfb69401f1cce74}//
+
+
+///////////////////////////////////////////
+//let weatherSection = document.querySelector('');
+
+
+//7ae51efa9b789abd3dfb69401f1cce74 my api code

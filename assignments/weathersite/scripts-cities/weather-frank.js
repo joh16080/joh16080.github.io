7617fc535ffbd3cc41a4623893473e8227c07163
Heather Johnson <joh16080@byui.edu>
2018-06-28 14:04:16 -0600

    update

---
+++ assignments/weathersite/scripts-cities/weather-frank.js
@@ -0,0 +1,25 @@
+//connection to the API//
+let weatherRequest = new XMLHttpRequest();
+let apiURL = 'https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=7ae51efa9b789abd3dfb69401f1cce74';
+weatherRequest.open('GET', apiURL, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+  let weatherData = JSON.parse(weatherRequest.responseText);
+  
+  console.log(weatherData);
+ 
+  document.getElementById('hightemp').innerHTML =  weatherData.main.temp_max;
+  
+  document.getElementById('humidity').innerHTML =  weatherData.main.humidity;
+  
+  document.getElementById('windspeed').innerHTML =  weatherData.wind.speed;
+  
+  document.getElementById('clouds').innerHTML =  weatherData.clouds.all;
+  
+  document.getElementById('currentCond').innerHTML =  weatherData.weather[0].description;
+  
+  let imagesrc = 'https://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+  document.getElementById('weatherimage').src = imagesrc;
+  
+}

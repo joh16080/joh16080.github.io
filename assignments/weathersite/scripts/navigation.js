e71ad289febea2c6cc6701c16655da6c19d1ef36
Heather Johnson <joh16080@byui.edu>
2018-07-01 16:44:30 -0600

    trying to fix

--- assignments/weathersite/scripts/navigation.js
+++ assignments/weathersite/scripts/navigation.js
@@ -3,14 +3,14 @@ var urlString = document.location.href;
 var urlArray = urlString.split('/');
 var pageHREF = urlArray[urlArray.length-1];
 
-if (pageHREF !=="") {
+if (pageHREF !== "") {
   var menu = document.querySelectorAll('ul#mainmenu li a');
   var i;
   //loop through all the menu items in the navigation
   for(i=0; i<menu.length; i++) {
     var currentURL = (menu[i].getAttribute("href"));
     menu[i].parentNode.className=""; //turn off highlighting by default
-    if (currentURL===pageHREF) {
+    if (currentURL === pageHREF) {
       menu[i].parentNode.className="active"; //turn on if a match
     } 
   } 
